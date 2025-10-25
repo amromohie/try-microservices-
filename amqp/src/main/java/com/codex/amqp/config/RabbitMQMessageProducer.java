c35726f6ea99e8105c32be64a5aa901cc0b66657
amromohie <damrmohy@gmail.com>
2025-10-25 11:43:39 +0300

    docker

--- amqp/src/main/java/com/codex/amqp/config/RabbitMQMessageProducer.java
+++ amqp/src/main/java/com/codex/amqp/config/RabbitMQMessageProducer.java
@@ -1,6 +1,5 @@
 package com.codex.amqp.config;
 
-import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.amqp.core.AmqpTemplate;
 import org.springframework.beans.factory.annotation.Qualifier;
@@ -8,7 +7,6 @@ import org.springframework.stereotype.Component;
 
 @Component
 @Slf4j
-
 public class RabbitMQMessageProducer {
   private final AmqpTemplate amqpTemplate;
 
@@ -18,9 +16,16 @@ public class RabbitMQMessageProducer {
 
   // to publish messages to the queue
   public void publish(Object payload, String exchange, String routingKey) {
-    log.info("Publishing message to exchange: {} with routing key: {} payload: {}", exchange, routingKey,payload);
+    log.info(
+        "Publishing message to exchange: {} with routing key: {} payload: {}",
+        exchange,
+        routingKey,
+        payload);
     amqpTemplate.convertAndSend(exchange, routingKey, payload);
-    log.info("Published message to exchange: {} with routing key: {} payload: {}", exchange, routingKey,payload);
-
+    log.info(
+        "Published message to exchange: {} with routing key: {} payload: {}",
+        exchange,
+        routingKey,
+        payload);
   }
 }
